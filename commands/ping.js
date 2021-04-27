90e20afe60e415a4e4e4406f51c5308cfb859607
datsfilipe <datsfilipe@gmail.com>
2021-04-27 12:23:13 -0300

    added ping.js command and message event for true - errors solved

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,14 @@
-module.exports = {
+// processing command
+module.exports.run = async (bot, message, params)=> {
+  // passing info log for console
+  console.log(`the user ${message.author.username} is running command ping.js`);
+  message.channel.send("Getting . . .").then ( m => {
+    m.edit(`the actual ping is ${Date.now() - m.createdTimestamp}`)
+  }).catch(console.error);
+};
+// exporting config for help.js
+module.exports.config = {
   "name": "ping",
   "cooldown": 10,
   "description": "the command show the actual ping",
-  async execute(message) {
-    message
-    .reply(`the actual ping is ${Math.round(message.client.ws.ping)}`)
-    .catch(console.error);
-  },
 };
--- events/message.js
+++ events/message.js
@@ -1,17 +1,13 @@
-module.exports = async (message, bot) => {
+module.exports = async (bot, message) => {
   // verifying if command is from a valid user
   if (message.author.bot) return;
-  // MESSAGE.AUTHOR.BOT IS UNDEFINED
   if (!message.content.startsWith(process.env.PREFIX)) return;
   // taking command from message
   let message_command = message.content.split(' ')[0].slice(process.env.PREFIX.length);
   let params = message.content.split(' ').slice(1);
   // verifying if command exists
-  if (bot.commands.has(message_command)) {
-    command = bot.commands.get(message_command);
-  } else {
-    return;
-  };
+  if (!bot.commands.has(message_command)) return;
+  command = bot.commands.get(message_command);
   // running command
-  command.run(bot, message, params);
+  command.run(bot, message, params)
 };
